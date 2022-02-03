8d066fdebe97f17ce09e71f49dad1400ee52cc06
Brooks Rosenberg <bjtr@bu.edu>
2022-02-03 11:57:52 -0500

    working APIS

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 var input = document.querySelector("#start");
 var Btn = document.querySelector(".button");
 var horoscopeDetails = document.querySelector(".horoscopeDetails");
-
+var inputValue = document.querySelector(".inputValue");
 console.log(input);
 
 //     Btn.addEventListener('click', function () {
@@ -25,18 +25,32 @@ input.addEventListener('change', function () {
         });
 
     // fun fact gen
-
+    
+});
     Btn.addEventListener('click', function () {
 
-            fetch("https://ffa.aakhilv.me/json", {
-                "method": "GET",
-            })
-                .then(response => {
-                    console.log(response.json());
-                })
-                .catch(err => {
-                    console.error(err)
-                });
+        fetch("https://baby-names-finder.p.rapidapi.com/detail/"+inputValue.value, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "baby-names-finder.p.rapidapi.com",
+		"x-rapidapi-key": "23498756d1msh8f2c810f2e947a2p133e8ejsn204782b3f173"
+	}
+})
+.then(response => {
+	console.log(response.json());
+})
+.catch(err => {
+	console.error(err);
+});
+            // fetch("https://ffa.aakhilv.me/json", {
+            //     "method": "GET",
+            // })
+            //     .then(response => {
+            //         console.log(response.json());
+            //     })
+            //     .catch(err => {
+            //         console.error(err)
+            //     });
         })
-    })
+    
 
